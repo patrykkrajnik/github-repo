514af60917ef737e899995e2335056bfdc0c6aee
patrykkrajnik <patrykkrajnik@icloud.com>
2021-01-14 14:32:06 +0100

    Storyboard removed

--- GitHub Repo/GitHub Repo/SearchController.swift
+++ GitHub Repo/GitHub Repo/SearchController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchController.swift
 //  GitHub Repo
 //
 //  Created by Patryk Krajnik on 14/01/2021.
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
